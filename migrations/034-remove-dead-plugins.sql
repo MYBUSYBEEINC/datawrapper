025a4480dc74dee890e699c42bf657371f4740e0
David Kokkelink <david@datawrapper.de>
2023-02-07 17:31:46 +0100

    fix(migrations): Fix plugin list

--- migrations/034-remove-dead-plugins.sql
+++ migrations/034-remove-dead-plugins.sql
@@ -1,10 +1,10 @@
 -- Removes old plugins from database
 
 -- Up
-DELETE FROM product_plugin WHERE plugin_id IN ('analytics-piwik', 'signin', 'signin-github', 'signin-twitter', 'signin-microsoft', 'visualization', 'onetime-access', 'chart-locale-select', 'visualization-raphael-chart');
-DELETE FROM plugin_organization WHERE plugin_id IN ('analytics-piwik', 'signin', 'signin-github', 'signin-twitter', 'signin-microsoft', 'visualization', 'onetime-access', 'chart-locale-select', 'visualization-raphael-chart');
-DELETE FROM plugin_data WHERE plugin_id IN ('analytics-piwik', 'signin', 'signin-github', 'signin-twitter', 'signin-microsoft', 'visualization', 'onetime-access', 'chart-locale-select', 'visualization-raphael-chart');
-DELETE FROM plugin WHERE id IN ('analytics-piwik', 'signin', 'signin-github', 'signin-twitter', 'signin-microsoft', 'visualization', 'onetime-access', 'chart-locale-select', 'visualization-raphael-chart');
+DELETE FROM product_plugin WHERE plugin_id IN ('analytics-piwik', 'signin', 'signin-github', 'signin-twitter', 'signin-microsoft', 'signin-google', 'visualization', 'onetime-access', 'chart-locale-select', 'visualization-raphael-chart');
+DELETE FROM plugin_organization WHERE plugin_id IN ('analytics-piwik', 'signin', 'signin-github', 'signin-twitter', 'signin-microsoft', 'signin-google', 'visualization', 'onetime-access', 'chart-locale-select', 'visualization-raphael-chart');
+DELETE FROM plugin_data WHERE plugin_id IN ('analytics-piwik', 'signin', 'signin-github', 'signin-twitter', 'signin-microsoft', 'signin-google', 'visualization', 'onetime-access', 'chart-locale-select', 'visualization-raphael-chart');
+DELETE FROM plugin WHERE id IN ('analytics-piwik', 'signin', 'signin-github', 'signin-twitter', 'signin-microsoft', 'signin-google', 'visualization', 'onetime-access', 'chart-locale-select', 'visualization-raphael-chart');
 
 -- Down
 DROP TABLE `chart_access_token`;
